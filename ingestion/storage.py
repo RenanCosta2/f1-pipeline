d9f7ab5cdffed7962b84ae2448693e7363652a2a
Renan Costa <105220100+RenanCosta2@users.noreply.github.com>
2026-06-01 16:16:53 -0300

    feat(ingestion): implement S3 upload for F1 ingestion data

---
+++ ingestion/storage.py
@@ -0,0 +1,57 @@
+import boto3
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+logger = logging.getLogger("f1_pipeline.ingestion.storage")
+
+
+class S3Uploader:
+    """
+    Storage class for uploading files to S3.
+
+    Attributes:
+        s3 (boto3.client): The S3 client.
+        bucket_name (str): The name of the S3 bucket.
+    """
+
+    def __init__(
+        self,
+        bucket_name: str,
+        AWS_REGION: str,
+        S3_ENDPOINT_URL: str,
+        AWS_ACCESS_KEY_ID: str,
+        AWS_SECRET_ACCESS_KEY: str,
+    ):
+        # Initialize the S3 client
+        self.s3 = boto3.client(
+            "s3",
+            region_name=AWS_REGION,
+            endpoint_url=S3_ENDPOINT_URL,
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        )
+        self.bucket_name = bucket_name
+
+    def upload_file(self, file_path: str, key: str) -> bool:
+        """
+        Uploads a file to S3.
+
+        Args:
+            file_path (str): The path to the file to upload.
+            key (str): The key (name) of the file in S3.
+
+        Returns:
+            bool: True if the file was uploaded successfully, False otherwise.
+        """
+        logger.info(f"Uploading file {file_path} to {key}")
+        try:
+            self.s3.upload_file(file_path, self.bucket_name, key)
+            logger.info("File uploaded successfully!")
+            return True
+        except Exception as e:
+            logger.error(f"Error uploading file {file_path}: {e}")
+            return False
